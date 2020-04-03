8dc8c3a1546761b279384b6e72b5e84057717a5a
Stevan Popovic <stevanpopovic5@gmail.com>
2020-04-03 14:05:32 +0100

    add YouTube link

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,17 @@ const IndexPage = () => (
 	<Layout>
 		<SEO title="Make your Staff and Workspace Happy" />
 
-		<div className={"page-header home"}>
-			<h1>Make your Staff and Workspace Happy</h1>
-			<p>HiStaff gives your complex the opportunity to increase the percentage of happiness<br />and enjoyment of your staff and as a result, bring productivity to your workspace.</p>
-			{/* <img alt={"Dashboard"} src={featureImage}/> */}
+		<div className="home container">
+			<div>
+				<h1>Make your Staff and Workspace Happy</h1>
+				<p>HiStaff gives your complex the opportunity to increase the percentage of happiness and enjoyment of your staff and as a result, bring productivity to your workspace.</p>
+			</div>
+			<div>
+				<iframe height="315" src="https://www.youtube.com/embed/FCgy0dphOGc?modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
 		</div>
 
-		<div className={"container"}>
-			{/* stuff */}
-		</div>
-		
+
 	</Layout>
 )
 
