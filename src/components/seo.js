b7ac30878477f112a415241a36b49e6255d6a97b
Stevan Popovic <stevanpopovic5@gmail.com>
2020-04-06 19:30:43 +0100

    diff path

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import logo from "../../public/images/showmethemoney.png"
+import logo from "../../static/images/showmethemoney.png"
 
 console.log(logo);
 
